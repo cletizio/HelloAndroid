701413c641055d81c4e9c42421191d37ada0e027
Chris Letizio <cletizio@gmail.com>
2024-04-19 22:49:15 -0400

    Added to MainActivity.kt
    Removed unused import, added background color and padding to improve asthetic.

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.helloandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.helloandroid.ui.theme.HelloAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +34,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(){
+    Surface(color = Color.Green){
         Text(
             text = "Hello $name!",
-            modifier = modifier
+            modifier = modifier.padding(32.dp)
         )
     }
 }
