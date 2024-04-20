f0c504e667900dc5bc0be9e31b2bf986df440ccc
Chris Letizio <cletizio@gmail.com>
2024-04-20 12:50:05 -0400

    Removed Green background, added new images, Formatted the words "Hello, Android!" to improve aesthetics.

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -3,17 +3,27 @@ package com.example.helloandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.helloandroid.ui.theme.HelloAndroidTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +34,10 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                )
+                {
+                    GreetingImage(name = stringResource(R.string.hello_android))
+
                 }
             }
         }
@@ -33,11 +45,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Green){
+fun GreetingText(name: String, modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Center, modifier = modifier){
         Text(
-            text = "Hello, $name!",
-            modifier = modifier.padding(32.dp)
+            text = name,
+            fontSize = 50.sp,
+            lineHeight = 120.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(name: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.wp2855634_background_hd_app)
+    //Create a box for the text
+    Box(modifier){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
+        )
+        GreetingText(name = name, modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
         )
     }
 }
@@ -46,6 +79,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HelloAndroidTheme {
-        Greeting("Android")
+        GreetingImage(name = stringResource(R.string.hello_android))
     }
 }
